49c06ccf4ff18a9eb799a7628c66d37e8fc8761e
Mark Slosarek <mark@slosarek.com>
2018-03-12 12:58:00 -0500

    Added support for config file reading and writing

--- switcher.js
+++ switcher.js
@@ -35,7 +35,16 @@ class Switcher {
       throw new Error(`Incorrectly formatted credentials file found in ${this.awsDir}`);
     }
 
-    this.profileData = this._getCredentials(this.awsDir);
+    const configResult = this._checkConfigFile(this.awsDir);
+    if (configResult === 'good') {
+      this.processConfig = true;
+    } else {
+      this.processConfig = false;
+    }
+
+    this.profileData = this._getCredentials().then(profileData => {
+      return this._getConfig(profileData);
+    });
   }
 
   /**
@@ -109,7 +118,6 @@ class Switcher {
   /**
    * Checks that the credentials file exists
    *
-   * @param {String} directory - Path to the aws directory
    * @returns {Boolean} - True if 'credentials' file is found
    */
   _checkDirectory() {
@@ -134,9 +142,28 @@ class Switcher {
   }
 
   /**
+   * Checks that the config file has valid data
+   *
+   * @returns {String} - Status of the file's contents
+   */
+  _checkConfigFile() {
+    if (!fs.existsSync(`${this.awsDir}/config`)) {
+      return 'bad';
+    }
+    const contents = fs.readFileSync(`${this.awsDir}/config`, 'utf8');
+    if (contents === '') {
+      return 'empty';
+    }
+    const regexMatch = contents.match(/\[.*\]\n* *output *= *.*\n *region *= *.*/);
+    if (regexMatch) {
+      return 'good';
+    }
+    return 'bad';
+  }
+
+  /**
    * Parses the credentials file into an Array of profile objects
    *
-   * @param {String} directory - Path to the aws directory
    * @returns {Promise.<Array>} - Array of profile objects
    * @reject {Error} - fs.readFile error
    */
@@ -202,25 +229,115 @@ class Switcher {
   }
 
   /**
+   * Parses the config file into an Array of profile objects,
+   * appends the config settings to the existing profile object
+   *
+   * @param {array} Profile objects - Existing array or profile objects
+   * @returns {Promise.<Array>} - Array of profile objects
+   * @reject {Error} - fs.readFile error
+   */
+  _getConfig(profileData) {
+    if (this.processConfig) {
+      return new Promise((resolve, reject) => {
+        fs.readFile(`${this.awsDir}/config`, 'utf8', (err, data) => {
+          if (err) {
+            return reject(err);
+          }
+
+          // Separate into lines
+          data = data.split('\n');
+
+          // Reduce the lines into an array of profile objects
+          data = data.reduce((prev, curr) => {
+            // Skip blank lines in the file
+            if (curr === '') {
+              return prev;
+            }
+
+            // This regex will match [.*] and extract interior characters as the second array item
+            // matches[0] = [AAAA]
+            // matches[1] = AAAA
+            const matches = curr.match(/\[(.*)\]/);
+
+            // If this is a profile name, push a new profile object to the array with the name
+            if (matches) {
+              // The config file user the pattern [profile profileName], need to strip the word profile
+              const [, name] = matches.map(match => match.replace(/profile/i, '').trim());
+              const existingProfile = prev.find(p =>
+                p.name !== undefined &&
+                Object.keys(p).length < 5
+              );
+              if (!existingProfile) {
+                prev.push({ name });
+              }
+              return prev;
+            }
+
+            /*
+            * At this point this line is neither blank nor a profile name
+            * Which means it's either an AWS profile ID or a configuration which are are formatted like:
+            *  output = OUTPUT
+            *  region = REGION
+            * With spaces in between the '=' and the key/value
+            */
+
+            // Split on '=' and then remove any spaces
+            // [0] === 'output'  || [0] === 'region'
+            // [1] === OUTPUT    || [1] === REGION
+            const [key, value] = curr.split('=').map(section => section.replace(' ', ''));
+
+            // Find an existing array object with a defined name and less then 5 object properties
+            // Each profile object should end up with 5 properties:
+            // [name, aws_access_key_id, aws_secret_access_key, region, output]
+            // Only the most recent profile object will have a defined name and less then 5 properties
+            const profile = prev.find(p =>
+              p.name !== undefined &&
+              Object.keys(p).length < 5
+            );
+
+            profile[key] = value;
+            return prev;
+          }, profileData || []); // Pass in the existing profileDate of an empty array to start the reduce
+          return resolve(data);
+        });
+      });
+    }
+    return Promise.resolve(profileData);
+  }
+
+  /**
    * Writes the credentials data back to the credentials file
    *
    * @param {Object[]} profile - Array of profile objects
    * @param {String} profile.name                   - The profile's name (no square brackets)
    * @param {String} profile.aws_access_key_id      - The profile's access key ID (from AWS)
    * @param {String} profile.aws_secret_access_key  - The profile's secret access key (from AWS)
+   * @param {String} profile.output  - The profile's output type
+   * @param {String} profile.region  - The profile's region location
    * @returns {Promise.<undefined>} - Returns an empty resolved Promise
    * @reject {Error} - Error writing the credentials file
    */
   _writeBackCredentialsData(data) {
     let writeData = '';
+    let configData = '';
 
     data.forEach(profile => {
       writeData = writeData.concat(`[${profile.name}]\n`);
       writeData = writeData.concat(`aws_access_key_id = ${profile.aws_access_key_id}\n`);
       writeData = writeData.concat(`aws_secret_access_key = ${profile.aws_secret_access_key}\n\n`);
+      if (this.processConfig) {
+        configData = configData.concat(`[profile ${profile.name}]\n`);
+        if (profile.output) {
+          configData = configData.concat(`output = ${profile.output}\n`);
+        }
+        if (profile.region) {
+          configData = configData.concat(`region = ${profile.region}\n`);
+        }
+        configData = configData.concat(`\n`);
+      }
     });
 
-    return new Promise((resolve, reject) => {
+    const credentialPromise = new Promise((resolve, reject) => {
       fs.writeFile(`${this.awsDir}/credentials`, writeData, err => {
         if (err) {
           return reject(err);
@@ -228,6 +345,20 @@ class Switcher {
         return resolve();
       });
     });
+    const configPromise = new Promise((resolve, reject) => {
+      if (this.processConfig) {
+        fs.writeFile(`${this.awsDir}/config`, configData, err => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve();
+        });
+      } else {
+        return resolve();
+      }
+    });
+
+    return Promise.all([credentialPromise, configPromise]);
   }
 
   /**
